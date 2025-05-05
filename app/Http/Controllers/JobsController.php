249878c00c9368b5d4db53aec6190f55bac84060
Md. Fahim Muntasir <srabon@Srabons-MacBook-Air.local>
2025-05-06 01:53:36 +0600

    jobs page has been made real-time and dynamic

--- app/Http/Controllers/JobsController.php
+++ app/Http/Controllers/JobsController.php
@@ -1,13 +1,26 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Category;
+use App\Models\JobType;
+use App\Models\Job;
+
 
 use Illuminate\Http\Request;
 
 class JobsController extends Controller
 {
     public function index(){
-
-        return view('front.jobs');
+        $categories = Category::where('status', 1)->get();
+        $jobTypes = JobType::where('status', 1)->get();
+        $jobs=Job::where('status',1)->orderBy('created_at', 'DESC')->paginate(9);
+        // Pass data to the view
+        return view('front.jobs', [
+            'categories' => $categories,  // Corrected this line
+            'jobTypes' => $jobTypes,
+            'jobs'=>$jobs,
+         
+        ]);
     }
+
 }
